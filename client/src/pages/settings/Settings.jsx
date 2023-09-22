51d11632a9053f61d909cf5a95dffd007e5e70b1
Aman <amantechmsy@gmail.com>
2023-09-23 02:21:55 +0530

    files upload

---
+++ client/src/pages/settings/Settings.jsx
@@ -0,0 +1,127 @@
+import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
+
+export default function Settings() {
+  const [file, setFile] = useState(null);
+  const [profilePic, setprofilePic] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/"
+
+  useEffect(()=>{
+    fetchCurrentUser();
+  },[])
+
+  const fetchCurrentUser=async()=>{
+    try {
+      const res = await axios.get('/users/'+user._id);
+      // console.log(res.data);
+      setEmail(res.data.email);
+      setUsername(res.data.username);
+      setprofilePic(res.data.profilePic);
+      setPassword(res.data.password);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "UPDATE_START" });
+    let updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password,
+    };
+    if (file) {
+            const data = new FormData();
+            data.append("file", file);
+            data.append(
+                "upload_preset",'chit-chat-pics'
+            );
+            data.append("cloud_name", 'aman-msy');    
+    
+            const uploadRes = await axios.post(
+              `https://api.cloudinary.com/v1_1/aman-msy/image/upload`,
+              data
+            );
+            const { url } = uploadRes.data; //Getting image url for storing to mongo
+            console.log(url);
+            updatedUser.profilePic=url; 
+    }
+    try {
+      const res = await axios.put("/users/" + user._id, updatedUser);
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "UPDATE_FAILURE" });
+    }
+  };
+
+
+  return (
+    <div className="settings">
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm" onSubmit={handleSubmit}>
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src={file ? URL.createObjectURL(file) : profilePic}
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </div>
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="settingsSubmit" type="submit">
+            Update
+          </button>
+          {success && (
+            <span
+              style={{ color: "green", textAlign: "center", marginTop: "20px" }}
+            >
+              Profile has been updated...
+            </span>
+          )}
+        </form>
+      </div>
+      <Sidebar />
+    </div>
+  );
+}
---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+dotenv.config();
+app.use(express.json());
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(
+  cors({
+    origin: ['https://650df8304dc514331077f6a7--dulcet-pasca-6e0d3e.netlify.app/'],    
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect('mongodb+srv://amantechmsy:GU92YuBSGXVAUGDg@cluster0.y35mqbz.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify:false
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen("5000", () => {
+  console.log("Backend is running.");
+});
