20a6eb1f2a0914bd2d1047b6875a2982fa38290b
Aman <amantechmsy@gmail.com>
2023-09-23 03:13:59 +0530

    modification

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,7 +4,7 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {post.length>0 && posts.map((p) => (
+      {posts.length>0 && posts.map((p) => (
         <Post post={p} />
       ))}
     </div>
