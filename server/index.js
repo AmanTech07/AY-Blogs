a047d6f2d099b678888e22689e6deafdabccdd07
Aman <amantechmsy@gmail.com>
2023-09-23 02:39:38 +0530

    modified files

--- server/index.js
+++ server/index.js
@@ -19,6 +19,22 @@ app.use((req, res, next) => {
   next();
 });
 
+// -----------------------
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV==='production') {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// ---------------------------
+
 app.use(
   cors({
     origin: ['https://650df8304dc514331077f6a7--dulcet-pasca-6e0d3e.netlify.app/'],    
